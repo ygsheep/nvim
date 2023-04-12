dc9625e4d9e6b2df227e0b6b0b7872641f00af77
yysheep <2741376563@qq.com>
2023-04-12 20:51:07 +0800

    修改默认文件夹为build

--- lua/dap/cmake-tools.lua
+++ lua/dap/cmake-tools.lua
@@ -7,8 +7,8 @@ local cmake = require("cmake-tools")
 cmake.setup {
   cmake_command = "cmake",
   cmake_build_directory = "",
-  cmake_build_directory_prefix = "cmake_build_", -- when cmake_build_directory is "", this option will be activated
-  cmake_generate_options = { "-D", "CMAKE_EXPORT_COMPILE_COMMANDS=1" },
+  cmake_build_directory_prefix = "build_", -- when cmake_build_directory is "", this option will be activated
+  cmake_generate_options = { "-D", "CMAKE_TOOLCHAIN_FILE=/home/yysheep/workspace/vcpkg/scripts/buildsystems/vcpkg.cmake " }, -- CMAKE_EXPORT_COMPILE_COMMANDS=1
   cmake_build_options = {},
   cmake_console_size = 10, -- cmake output window height
   cmake_console_position = "belowright", -- "belowright", "aboveleft", ...
