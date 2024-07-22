d2b3d8c1622b9587d6a621e91145f8d4c6bec9d2
ygsheep <ygsheep@outlook.com>
2024-07-22 08:55:22 +0800

    markdown 预览插件

--- lua/plugins/markdown_preview.lua
+++ lua/plugins/markdown_preview.lua
@@ -4,6 +4,8 @@ return {
   build = "cd app && yarn install",
   init = function()
     vim.g.mkdp_filetypes = { "markdown" }
+    -- default browser
+    vim.g.mkdp_browser = "chromium"
   end,
   ft = { "markdown" },
 }
